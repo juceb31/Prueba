2d25bc6a56f185a62a52e302ff552b0c1d653f6e
Sarahi <sarahir007@gmail.com>
2019-07-09 21:16:13 -0600

    Esqueleto del proyecto

---
+++ FARMACY_PROJECT/FP_APP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FP_APP.MvcApplication" Language="C#" %>
---
+++ FARMACY_PROJECT/FP_DTO/BOLETA_DTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP_DTO
+{
+    public class BOLETA_DTO
+    {
+           public string num_boleta { get; set; }
+            public System.DateTime fecha { get; set; }
+            public string cod_empl { get; set; }
+            public string cod_cli { get; set; }
+            public string num_ordenpedido { get; set; }
+            public int subtotal { get; set; }
+            public Nullable<int> descuento { get; set; }
+            public int total { get; set; }
+
+        //    public virtual OrdenPedido OrdenPedido { get; set; }
+        
+    }
+}
